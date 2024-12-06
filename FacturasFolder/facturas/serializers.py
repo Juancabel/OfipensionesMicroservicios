3dd513437ab0d59fab84520dfe88cfc91b8e63d4
Juancabel <jc.beltrang1@uniandes.edu.co>
2024-12-06 11:14:20 -0500

    dfsdf

--- FacturasFolder/facturas/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from . import models
-
-
-class FacturaSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        fields = ('id', 'nombre', 'monto','metodo' ,'documento_identidad')
-        model = models.Factura
--- FacturasFolder/facturas/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
-
-from . import views
-
-urlpatterns = [
-    path('facturas/', views.facturas_list, name='facturaList'),
-    path('facturacreate/', csrf_exempt(views.crear_factura), name='facturaCreate'),
-]
