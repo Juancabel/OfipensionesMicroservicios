f7188871aece55e29507742237ff43fc95f3369d
Juancabel <jc.beltrang1@uniandes.edu.co>
2024-12-06 12:53:45 -0500

    Mongo Client

--- InformesFolder/informes/models/db.py
+++ InformesFolder/informes/models/db.py
@@ -3,7 +3,7 @@ from pydantic.functional_validators import BeforeValidator
 from typing_extensions import Annotated
 
 client = motor.motor_asyncio.AsyncIOMotorClient(
-    "mongodb://monitoring_user:isis2503@10.128.0.86:27017?retryWrites=true&w=majority"
+    "mongodb://monitoring_user:isis2503@10.128.0.70:27017"
 )
 db = client.get_database("monitoring_db")
 informes_collection = db.get_collection("informes")
